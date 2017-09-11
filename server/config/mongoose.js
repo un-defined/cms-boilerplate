bbe68fb4b40909bb5ba79a4230b1baa618276db1
un-defined <659168590@qq.com>
2017-09-11 22:41:54 +0800

    clear repository

--- server/config/mongoose.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import { config } from './index';
-
-export default function () {
-    mongoose.Promise = global.Promise;
-    let db = mongoose.connect(config.mongodb);
-    return db;
-}
--- server/index.js
+++
@@ -1,9 +0,0 @@
-
-var register = require('babel-register');
-
-register({
-    presets: ['es2015', 'stage-3']
-});
-require('babel-polyfill');
-require('./app.js');
-global.navigator = { userAgent: 'all' }
